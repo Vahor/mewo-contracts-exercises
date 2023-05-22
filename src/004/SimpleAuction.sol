edc0cdb4dd4491a65963c2fc0b9e35b475ef734e
nathan <nathan.d0601@gmail.com>
2023-05-22 10:38:39 +0200

    004

--- src/004/SimpleAuction.sol
+++ src/004/SimpleAuction.sol
@@ -7,4 +7,14 @@ pragma solidity ^0.8.20;
 // - a public function `placeBid` that allows users to place a bid by providing an amount.
 //   If the `highestBid` is higher than the bid provided by the sender, the bid should be ignored
 //   (e.g. highestBid remains unchanged).
-contract SimpleAuction { }
+contract SimpleAuction {
+
+    uint256 public highestBid;
+
+    function placeBid(uint256 bid) public {
+        if (highestBid > bid) return;
+        highestBid = bid;
+    }
+
+
+}
