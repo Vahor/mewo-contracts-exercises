ef59289aa433853e59e9f833c8fa352db9163f0b
nathan <nathan.d0601@gmail.com>
2023-05-22 10:35:12 +0200

    003

--- src/003/SimpleBank.sol
+++ src/003/SimpleBank.sol
@@ -7,4 +7,18 @@ pragma solidity ^0.8.20;
 // Users can interact with the bank account using two functions.
 // - The deposit function should take an `amount` as a parameter and increase the account `balance` by that `amount`.
 // - The withdraw function should take an `amount` and decrease the account `balance` by that `amount` if sufficient funds are available (e.g. `balance` > 0).
-contract SimpleBank { }
+contract SimpleBank {
+
+    uint256 public balance;
+
+    function deposit(uint256 amount) public {
+        balance += amount;
+    }
+
+    function withdraw(uint256 amount) public {
+        require(balance >= amount, "Insufficient amount");
+        balance -= amount;
+    }
+
+
+}
