f10ab4e71d80e96d8b28c662f32f297f8254af5c
nathan <nathan.d0601@gmail.com>
2023-05-22 14:50:59 +0200

    maybe?

--- src/010/Crowdsale.sol
+++ src/010/Crowdsale.sol
@@ -2,12 +2,13 @@
 pragma solidity ^0.8.20;
 
 import { IERC20 } from "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+import "openzeppelin-contracts/contracts/access/Ownable.sol";
 
 // Create a contract that allow users to buy an ERC20 (Mewo) tokens with ethers.
 // The Crowdsale contract will automatically receive 1,000,000,000 tokens (e.g mewo.balanceOf(address(Crowdsale)) == 1e9).
 // Token will be sold with the rate: 1 ether = 10000 tokens.
 // As always, add withdraw functiononly callable the owner of the contract but use the Ownable contract from the OpenZeppelin library.
-contract Crowdsale {
+contract Crowdsale is Ownable {
   uint256 public constant RATE = 10000;
   IERC20 private token;
 
@@ -17,5 +18,9 @@ contract Crowdsale {
 
   function buy() public payable {
     token.transfer(msg.sender, msg.value * RATE  );
+  }
+
+  function withdraw() public onlyOwner {
+    msg.sender.transfer(payable(this).balance); //?
   }
 }
